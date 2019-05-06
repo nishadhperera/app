9273be13161b6e17239e40f14478b7f206033831
Nishadh Aluthge <nishadh.aluthge@sky.uk>
2019-05-06 20:10:41 +0100

    Modified the url patterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
-    path('', views.signup),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
 ]
 
