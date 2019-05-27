c7a4c5daa6d2f657c6f79b8b2809487ade73b8b2
Nishadh Aluthge <nishadh.aluthge@sky.uk>
2019-05-27 12:50:44 +0100

    Modified the main URL structure

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('articles.urls')),
+    path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
+    path('doctor/', include('doctor.urls')),
+    path('patient/', include('patient.urls')),
     path('admin/', admin.site.urls),
 ]
